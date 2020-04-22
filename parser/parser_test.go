8b86e3061b585a8136b0cec32297a14aa1c8cb94
alitaso345 <alice.maru345@gmail.com>
2020-04-22 22:11:20 +0900

    add HashLiteral parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -836,6 +836,25 @@ func TestParsingIndexExpressions(t *testing.T) {
 	}
 }
 
+func TestParsingEmptyHashLiteral(t *testing.T) {
+	input := "{}"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt := program.Statements[0].(*ast.ExpressionStatement)
+	hash, ok := stmt.Expression.(*ast.HashLiteral)
+	if !ok {
+		t.Fatalf("exp is not ast.HashLiteral. got=%T", stmt.Expression)
+	}
+
+	if len(hash.Pairs) != 0 {
+		t.Errorf("hash.Pairs has wrong length. got=%d", len(hash.Pairs))
+	}
+}
+
 func TestParsingHashLiteralsStringKeys(t *testing.T) {
 	input := `{"one": 1, "two": 2, "three": 3}`
 
